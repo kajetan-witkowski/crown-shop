9ec4a2aa73996ac72717d8abf38d3f7c3693ebec
udemy535 <udemy535@gmail.com>
2020-09-13 14:39:10 +0200

    Adding header component, logo and link to shop and contact page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./components/shop-component/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
